1d242c7a821f05b3ac53691dacf38505b1fa5ad8
Michael Ahlers <michael@ahlers.co>
2013-01-18 17:07:46 -0500

    Basic image viewer grid.

--- public/controllers/Sponsors.js
+++ public/controllers/Sponsors.js
@@ -1,6 +1,27 @@
 'use strict'
 
-define(['controllers', 'services/Resource'], function (controllers) {
-  controllers.controller('Sponsors', function ($rootScope, $scope, Resource) {
+define([
+
+  'controllers',
+
+  'services/Resource'
+
+], function (controllers) {
+  controllers.controller('Sponsors', function ($rootScope, $scope, $log, Resource) {
+    $scope.sponsors = Resource.get({name : 'sponsors'})
+
+    $scope.getSponsorGroups = function () {
+      var sponsors = []
+
+      $.each($scope.sponsors.data || [], function (index, value) {
+        index = Math.floor(index / 4)
+        sponsors[index] = sponsors[index] || []
+        sponsors[index].push(value)
+      })
+
+      $log.log(sponsors)
+
+      return sponsors
+    }
   })
 })
