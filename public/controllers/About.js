11afd5476ef4bc83d8ee57b6b2ea5732a4e25520
Michael Ahlers <michael@ahlers.co>
2013-01-18 01:59:04 -0500

    Pulling in data from Facebook! And applying some basic formatting.

--- public/controllers/About.js
+++ public/controllers/About.js
@@ -2,5 +2,6 @@
 
 define(['controllers', 'services/Resource'], function (controllers) {
   controllers.controller('About', function ($rootScope, $scope, Resource) {
+    $scope.content = Resource.get({name : 'about'})
   })
 })
