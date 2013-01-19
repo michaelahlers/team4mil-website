05e8dc75f4731aaec6f70715db75fbad97e6d970
Michael Ahlers <michael@ahlers.co>
2013-01-18 20:16:08 -0500

    Not needed.

--- public/controllers/Sponsors.js
+++ public/controllers/Sponsors.js
@@ -11,34 +11,5 @@ define([
 ], function (controllers) {
   controllers.controller('Sponsors', function ($rootScope, $scope, $log, Resource) {
     $scope.sponsors = Resource.get({name : 'sponsors'})
-
-    $scope.getSponsorGroups = function () {
-      var sponsors = []
-
-      $.each($scope.sponsors.data || [], function (index, value) {
-        index = Math.floor(index / 4)
-        sponsors[index] = sponsors[index] || []
-        sponsors[index].push(value)
-      })
-
-      $log.log(sponsors)
-
-      return sponsors
-    }
-
-//    $scope.$watch('sponsors', function (outgoing, incoming) {
-//      $(function () {
-//
-//        var $container = $('#logos');
-//
-//        $container.imagesLoaded(function () {
-//          alert('')
-//          $container.masonry({
-//            itemSelector : 'img'
-//          })
-//        })
-//
-//      })
-//    })
   })
 })
