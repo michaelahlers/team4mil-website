3150ef8617bd7d3b980ae3dc298cdd6b9db4a2e9
Michael Ahlers <michael@ahlers.co>
2013-01-19 12:58:22 -0500

    Refactoring and introducing the Teams view.

---
+++ public/controllers/Teams.js
@@ -0,0 +1,18 @@
+'use strict'
+
+define([
+
+  'controllers',
+
+  'services/Cache',
+  'services/Resource'
+
+], function (controllers) {
+
+  controllers.controller('Teams', function ($rootScope, $scope, $log, Cache, Resource) {
+    //    $scope.content = Cache.get('teams', function () {
+    //      return Resource.get({name : 'teams'})
+    //    })
+  })
+
+})
