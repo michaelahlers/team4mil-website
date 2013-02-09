b38d3265f7cb98d7ae5afc4d701c1d2f514dce3e
Michael Ahlers <michael@ahlers.co>
2013-02-09 12:54:31 -0500

    Completing the workflow.

--- public/controllers/Contact.js
+++ public/controllers/Contact.js
@@ -16,36 +16,35 @@ define([
     })
   })
 
-  controllers.controller('ContactMessage', function ($rootScope, $scope, $log, $http) {
-    $scope.recipient = {
+  controllers.controller('ContactMessage', function ($rootScope, $scope, $log, $timeout, $http) {
+    var defaultRecipient = $scope.recipient = {
       mail : 'contact@team4il.org'
     }
 
-    var findRecipient = function (mail) {
-      if (!$scope.content || !$scope.content.board) {
-        return undefined
-      }
-
-      return $.grep($scope.content.board.members, function (member) {
-        return member.mail == mail
-      })[0]
-    }
+//var findRecipient = function (mail) {
+//  if (!$scope.content || !$scope.content.board) {
+//    return undefined
+//  }
+//
+//  return $.grep($scope.content.board.members, function (member) {
+//    return member.mail == mail
+//  })[0]
+//}
 
     $scope.send = function () {
 
-      $scope.status = {
-        message : 'Sending your message to ' + $scope.recipient.mail + '.'
+      var closeStatus = $scope.closeStatus = function () {
+        $('#modalStatus').modal('hide')
       }
 
-//      if (current.id) {
+      $scope.status = {
+        pending : true
+      }
+
       $('#modalStatus').modal({
         backdrop : 'static',
         keyboard : false
       })
-//      } else {
-//        $('#modalBiography').modal('hide')
-//        $location.search({})
-//      }
 
       $http.post('/contact', {
         message : {
@@ -56,10 +55,21 @@ define([
         }
       })
         .success(function () {
+          $scope.status = {
+            success : true
+          }
 
+          $scope.recipient = defaultRecipient
+          delete $scope.sender
+          delete $scope.subject
+          delete $scope.body
+
+          $timeout(closeStatus, 5000)
         })
         .error(function () {
-
+          $scope.status = {
+            error : true
+          }
         })
     }
   })
