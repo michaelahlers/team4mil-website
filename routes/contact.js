220a2b509b0e25e035b579ffe0de3994a108ab90
Michael Ahlers <michael@ahlers.co>
2013-02-08 20:06:32 -0500

    Trying something else.

--- routes/contact.js
+++ routes/contact.js
@@ -1,30 +1,46 @@
 /*jshint node:true*/
 'use strict'
 
-var nodemailer = require('nodemailer')
+//var nodemailer = require('nodemailer')
+//
+//var transport = nodemailer.createTransport('SMTP', {
+//  service : 'Mailgun',
+//  auth : {
+//    user : 'api',
+//    pass : 'key-4vqyrivdws3fvkaj2d391ict3kbsinb5'
+//  }
+//})
+//
 
-var transport = nodemailer.createTransport('SMTP', {
-  service : 'Mailgun',
-  auth : {
-    user : 'api',
-    pass : 'key-4vqyrivdws3fvkaj2d391ict3kbsinb5'
-  }
-})
+var Mailgun = require('mailgun').Mailgun
+
+var transport = new Mailgun('key-4vqyrivdws3fvkaj2d391ict3kbsinb5')
 
 exports.send = function (req, res) {
 
-  var options = {
-    from : 'michael.ahlers@team4mil.org',
-    to : 'michael@ahlers.co',
-    subject : 'Hello!',
-    text : 'You suck.'
-  }
+  transport.sendText('michael@ahlers.co', [],
+    'This is the subject',
+    'This is the text',
+    'michael.ahlers@team4mil.org', {},
+    function (err) {
+      if (err) console.log('Oh noes: ' + err);
+      else     console.log('Success');
+    })
 
-  console.log('Sending mail.', options)
 
-  transport.sendMail(options, function (error, response) {
-    console.log(error || response)
-  })
+//
+//  var options = {
+//    from : 'michael.ahlers@team4mil.org',
+//    to : 'michael@ahlers.co',
+//    subject : 'Hello!',
+//    text : 'You suck.'
+//  }
+//
+//  console.log('Sending mail.', options)
+//
+//  transport.sendMail(options, function (error, response) {
+//    console.log(error || response)
+//  })
 
   res.json()
 
