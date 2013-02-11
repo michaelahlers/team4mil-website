e36617177c21a2f1362b1e6114486dbe3d1c518e
Michael Ahlers <michael@ahlers.co>
2013-02-11 12:02:16 -0500

    Externalize the RequireJS configuration so it may be used by the optimizer at production time.

---
+++ public/build.js
@@ -0,0 +1,76 @@
+var require = {
+  name : './index',
+  out : 'index.min.js',
+
+  paths : {
+    'angular' : 'lib/angular-1.1.1/angular',
+    'angular-bootstrap' : 'lib/angular-1.1.1/angular-bootstrap',
+    'angular-bootstrap-prettify' : 'lib/angular-1.1.1/angular-bootstrap-prettify',
+    'angular-loader' : 'lib/angular-1.1.1/angular-loader',
+    'angular-resource' : 'lib/angular-1.1.1/angular-resource',
+    'angular-sanitize' : 'lib/angular-1.1.1/angular-sanitize',
+    'bootstrap' : 'lib/bootstrap-2.2.2/js/bootstrap',
+    'domReady' : 'lib/domReady-2.0.1',
+    'jquery' : 'lib/jquery-1.8.3',
+    'jquery-gridster' : 'lib/gridster.js-master/dist/jquery.gridster.js',
+    'jquery-imagesLoaded' : 'lib/jquery.imagesloaded.min',
+    'jquery-masonry' : 'lib/masonry-master/jquery.masonry',
+    'modernizr' : 'lib/modernizr-2.6.1',
+    'text' : 'lib/text-2.0.3.js'
+  },
+
+  shim : {
+
+    'angular' : {
+      exports : 'angular'
+    },
+
+    'angular-bootstrap' : {
+      deps : ['angular', 'bootstrap']
+    },
+
+    'angular-bootstrap-prettify' : {
+      deps : ['angular', 'angular-bootstrap']
+    },
+
+    'angular-loader' : {
+      deps : ['angular']
+    },
+
+    'angular-resource' : {
+      deps : ['angular']
+    },
+
+    'angular-sanitize' : {
+      deps : ['angular']
+    },
+
+    'bootstrap' : {
+      deps : ['jquery']
+    },
+
+    'jquery' : {
+      /* jQuery does not depend on Angular JS, but it must be loaded
+       * afterwards to avoid subtle bugs. */
+      deps : ['angular'],
+      exports : '$'
+    },
+
+    'jquery-gridster' : {
+      deps : ['jquery']
+    },
+
+    'jquery-imagesLoaded' : {
+      deps : ['jquery']
+    },
+
+    'jquery-masonry' : {
+      deps : ['jquery', 'jquery-imagesLoaded']
+    }//,
+
+    //'jquery-ui' : {
+    //  deps : ['jquery']
+    //}
+
+  }
+}
