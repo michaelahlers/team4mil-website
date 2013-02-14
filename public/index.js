5a1151f6b78f5e9418d22b44ddf55a3df0b3b5f9
Michael Ahlers <michael@ahlers.co>
2013-02-14 17:58:02 -0500

    Duplicates what jQuery provides.

--- public/index.js
+++ public/index.js
@@ -1,20 +1,21 @@
 require([
 
-  'domReady!',
-  'angular',
   'jquery',
+  'angular',
 
   'bootstrap',
 
   /* The application itself. */
   'applications'
 
-], function (document, angular, $) {
+], function ($, angular) {
 
-  /* The requirements on this script ensure the application module has been
-   * defined prior to bootstrapping AngularJS. If this is done in the wrong
-   * order, there will be visible flicker as ngCloak is removed and the main
-   * application scope is digested. */
-  angular.bootstrap(document, ['app'])
+  $(function () {
+    /* The requirements on this script ensure the application module has been
+     * defined prior to bootstrapping AngularJS. If this is done in the wrong
+     * order, there will be visible flicker as ngCloak is removed and the main
+     * application scope is digested. */
+    angular.bootstrap(document, ['app'])
+  })
 
 })
