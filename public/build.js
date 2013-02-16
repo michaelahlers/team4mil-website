6c82fa6d162c18bae102985480ab35b5d02193ba
Michael Ahlers <michael@ahlers.co>
2013-02-15 20:29:36 -0500

    House cleaning.

--- public/build.js
+++ public/build.js
@@ -2,6 +2,7 @@
   /* See https://github.com/jrburke/r.js/blob/master/build/example.build.js for complete documentation on these options. */
 
   return window.require = module.exports = {
+    baseUrl : '/',
     name : './index',
     out : './index.production.js',
 
@@ -14,12 +15,9 @@
       'angular-sanitize' : 'lib/angular-1.1.2/angular-sanitize',
       'async' : 'lib/async',
       'bootstrap' : 'lib/bootstrap-2.2.2/js/bootstrap',
-      'domReady' : 'lib/domReady-2.0.1',
       'jquery' : 'lib/jquery-1.8.3',
       'jquery-gridster' : 'lib/gridster.js-master/dist/jquery.gridster.js',
       'jquery-imagesLoaded' : 'lib/jquery.imagesloaded',
-      'jquery-masonry' : 'lib/masonry-master/jquery.masonry',
-      'modernizr' : 'lib/modernizr-2.6.1',
       'text' : 'lib/text-2.0.3.js'
     },
 
@@ -66,12 +64,9 @@
 
       'jquery-imagesLoaded' : {
         deps : ['jquery']
-      },
-
-      'jquery-masonry' : {
-        deps : ['jquery', 'jquery-imagesLoaded']
-      }//,
-
+      }
+      //,
+      //
       //'jquery-ui' : {
       //  deps : ['jquery']
       //}
