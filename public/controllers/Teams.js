ad5059b8d15d703994fa2a9dad51ecd850ebdaa1
Michael Ahlers <michael@ahlers.co>
2013-02-16 12:53:26 -0500

    Buttons to cycle through the riders.

--- public/controllers/Teams.js
+++ public/controllers/Teams.js
@@ -28,63 +28,16 @@ define([
     $scope.$on('$routeChangeSuccess', selectMember)
     $scope.$on('$routeUpdate', selectMember)
 
-    /*
-     $scope.toRows = function (array, length) {
-     array = (array || []).slice(0)
+    $scope.getNext = function () {
+      var members = ($scope.$eval('team.members') || [])
+      return members[(members.indexOf($scope.$eval('member')) + 1) % members.length]
+    }
 
-     var rows = []
+    $scope.getPrevious = function () {
+      var members = ($scope.$eval('team.members') || [])
+      return members[(members.indexOf($scope.$eval('member')) + members.length - 1) % members.length]
+    }
 
-     while (array.length) {
-     rows.push(array.splice(0, length))
-     }
-
-     $log.log(rows)
-
-     return rows
-     }
-
-     $scope.$on('$routeUpdate', function () {
-     $scope.focused = {
-     id : $routeParams.id
-     }
-     })
-
-     $scope.$on('$routeChangeSuccess', function (event, current, previous) {
-     $scope.focused = {
-     id : $routeParams.id
-     }
-     })
-
-     $scope.$watch('content.teams + focused.id', function () {
-     if (!$scope.content.teams || !$scope.focused.id) {
-     return
-     }
-
-     var members = $.map($scope.content.teams, function (team) {
-     return team.members
-     })
-
-     $scope.focused = members.filter(function (member) {
-     return $scope.focused.id == member.id
-     })[0]
-     })
-
-     //    $('#modalBiography').on('hide', function () {
-     //      $scope.focused = {}
-     //    })
-
-     $scope.$watch('focused', function (current, previous) {
-     if (current.id) {
-     $('#modalBiography').modal({
-     backdrop : 'static',
-     keyboard : false
-     })
-     } else {
-     $('#modalBiography').modal('hide')
-     $location.search({})
-     }
-     })
-     */
   })
 
 })
