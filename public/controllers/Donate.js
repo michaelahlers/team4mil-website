febf400c6bcc34d25a10db832f228a113f8051fe
Michael Ahlers <michael@ahlers.co>
2013-02-17 11:24:38 -0500

    Advancing the donation form and starting to write controller logic.

--- public/controllers/Donate.js
+++ public/controllers/Donate.js
@@ -7,21 +7,44 @@ define([
 ], function (controllers) {
 
   return controllers.controller('Donate', function ($rootScope, $scope, $log) {
-    $scope.months = [
-      { index : 1, name : 'January' },
-      { index : 1, name : 'January' },
-      { index : 2, name : 'February' },
-      { index : 3, name : 'March' },
-      { index : 4, name : 'April' },
-      { index : 5, name : 'May' },
-      { index : 6, name : 'June' },
-      { index : 7, name : 'July' },
-      { index : 8, name : 'August' },
-      { index : 9, name : 'September' },
-      { index : 10, name : 'October' },
-      { index : 11, name : 'November' },
-      { index : 12, name : 'December'}
-    ]
+
+    var reset = function () {
+      var now = new Date()
+
+      $scope.donation = {
+        donor : {
+          name : '',
+          mail : ''
+        },
+        card : {
+          /* Sample credit card number for Stripe testing. */
+          number : '4242 4242 4242 4242',
+          expiration : {
+            month : now.getMonth() + 1,
+            year : now.getFullYear()
+          },
+          code : ''
+        }
+      }
+    }
+
+    reset()
+
+    $scope.resetDonation = function () {
+      reset()
+    }
+
+    $scope.startDonation = function () {
+
+    }
+
+    $scope.cancelDonation = function () {
+
+    }
+
+    $scope.confirmDonation = function () {
+
+    }
   })
 
 })
