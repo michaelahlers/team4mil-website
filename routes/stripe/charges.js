68f2750f27843e72a032a28825dd223854458fbf
Michael Ahlers <michael@ahlers.co>
2013-02-18 18:01:50 -0500

    Wire up the confirmation e-mail.

--- routes/stripe/charges.js
+++ routes/stripe/charges.js
@@ -1,13 +1,23 @@
 /*jshint node:true*/
 'use strict'
 
+var mailer = require('../../mailer.js')
+  , util = require('util')
+
 module.exports = function (stripe) {
   return {
+
     create : function (req, res) {
+
       var charge = {
         card : req.body.token,
         currency : req.body.currency,
         amount : req.body.amount
+      }
+
+      var payer = {
+        name : req.body.payer.name,
+        mail : req.body.payer.mail
       }
 
       stripe.charges.create(charge, function (err, result) {
@@ -16,8 +26,27 @@ module.exports = function (stripe) {
           return
         }
 
+        var message = {
+          sender : {
+            name : 'Team 4Mil Donations',
+            mail : 'donations@team4mil.org'
+          },
+
+          recipient : {
+            name : payer.name,
+            mail : payer.mail
+          },
+
+          subject : 'Thank you, from Team 4Mil, for your donation!',
+          body : util.format("We've processed your donation of $%s.00 to account ending %s.", charge.amount, result.card.last4)
+        }
+
+        mailer.send(message)
+
         res.json(result)
       })
+
     }
+
   }
 }
