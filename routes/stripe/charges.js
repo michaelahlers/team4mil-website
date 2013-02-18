1ec3ef2d179713ba849c5977ce2d8456a5dbf4f6
Michael Ahlers <michael@ahlers.co>
2013-02-18 12:46:41 -0500

    Consolidate Stripe initialization.

--- routes/stripe/charges.js
+++ routes/stripe/charges.js
@@ -1,23 +1,23 @@
 /*jshint node:true*/
 'use strict'
 
-var stripe = require('stripe')('sk_test_fxNypkThUlkUzMcEMR9Jj1cB')
+module.exports = function (stripe) {
+  return {
+    create : function (req, res) {
+      var charge = {
+        card : req.body.token,
+        currency : req.body.currency,
+        amount : req.body.amount
+      }
 
-exports.create = function (req, res) {
-  var charge = {
-    card : req.body.token,
-    currency : req.body.currency,
-    amount : req.body.amount
-  }
+      stripe.charges.create(charge, function (err, result) {
+        if (err) {
+          res.json(500, err)
+          return
+        }
 
-  console.log(charge)
-
-  stripe.charges.create(charge, function (err, result) {
-    if (err) {
-      res.json(500, err)
-      return
+        res.json(result)
+      })
     }
-
-    res.json(result)
-  })
+  }
 }
--- routes/stripe/index.js
+++ routes/stripe/index.js
@@ -1,6 +1,7 @@
 /*jshint node:true*/
 'use strict'
 
-var charges = require('./charges')
+var stripe = require('stripe')('sk_test_fxNypkThUlkUzMcEMR9Jj1cB')
+  , charges = require('./charges')(stripe)
 
 exports.charges = charges
