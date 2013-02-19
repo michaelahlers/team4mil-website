588488d39e08f48aa7ac62885935a2f7d6300f5a
Michael Ahlers <michael@ahlers.co>
2013-02-19 12:36:30 -0500

    Experimental version delegation.

---
+++ routes/articles/index.js
@@ -0,0 +1,36 @@
+/*jshint node:true*/
+'use strict'
+
+var version0 = require('./version0')
+
+var versions = [ version0 ]
+var latest = version0
+
+
+var getService = function (version) {
+  return versions[version] || latest
+}
+
+var getHandler = function (article, method, req, res) {
+  var service = getService(req.params.version)
+  return service[article][method]
+}
+
+exports.mission = {
+  get : getHandler('mission', 'get')
+}
+
+exports.teams = {
+  get : getHandler('teams', 'get')
+}
+
+exports.sponsorship = {
+  get : getHandler('sponsorship', 'get')
+}
+
+exports.contact = {
+  get : getHandler('contact', 'get')
+}
+
+exports.versions = versions
+exports.latest = latest
