a8a1da7b7f0ff756d1f8b6f1668c1dc72f06c4e7
Michael Ahlers <michael@ahlers.co>
2013-02-18 19:37:47 -0500

    Documentation.

--- routes/stripe/index.js
+++ routes/stripe/index.js
@@ -1,6 +1,8 @@
 /*jshint node:true*/
 'use strict'
 
+/* Visit https://github.com/abh/node-stripe for the Node Stripe API documentation. */
+
 var environment = require('../../environment')
   , stripe = require('stripe')(environment.STRIPE_SECRET_KEY)
   , charges = require('./charges')(stripe)
