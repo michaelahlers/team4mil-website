0c16bedc13baaeebc466ca354a06bd70d9c95758
Michael Ahlers <michael@ahlers.co>
2013-04-05 10:35:05 -0400

    We're going to differentiate between development and production.

--- environment.js
+++ environment.js
@@ -5,3 +5,5 @@ exports.MAILGUN_API_KEY = process.env.MAILGUN_API_KEY || 'key-4vqyrivdws3fvkaj2d
 
 exports.STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY || 'sk_test_G5yGsRwgUd2kdLH8ohyrKopm'
 exports.STRIPE_PUBLISHABLE_KEY = process.env.STRIPE_PUBLISHABLE_KEY || 'pk_test_2R7rJ6BRwTCbW6SW21jAGVr1'
+
+exports.GOOGLE_ANALYTICS_TRACKING_ID = process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'UA-38900711-2'
