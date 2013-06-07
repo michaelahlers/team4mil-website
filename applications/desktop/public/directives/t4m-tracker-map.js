93afcaf0b8ad36c8b90ed835080eb8228b0ee219
Michael Ahlers <michael@ahlers.co>
2013-06-07 16:30:20 -0400

    Successfully relocating the map el.

--- applications/desktop/public/directives/t4m-tracker-map.js
+++ applications/desktop/public/directives/t4m-tracker-map.js
@@ -6,17 +6,32 @@ define([
   , 'jquery'
 ], function (angular, directives, $) {
 
-  directives.directive('t4mTrackerMap', function ($log) {
+  directives.directive('t4mTrackerMap', function ($timeout, $log) {
     return {
       restrict : 'E',
       replace : true,
       template : '<div style="margin: 0; padding: 0; width: 100%; height: 100%;" />',
 
       link : function (scope, iEl, iAttrs, controller) {
-        $('<iframe class="map" style="margin: 0; padding: 0; width: 100%; height: 100%;" frameborder="0"></iframe>')
+        iEl.css({visibility : 'hidden'})
+
+        var frameEl = $('<iframe class="map" style="margin: 0; padding: 0; width: 100%; height: 100%;" frameborder="0"></iframe>')
           .appendTo(iEl)
           .load(function () {
-            alert('Loaded.')
+            frameEl.contents().find('#map').parent()
+              .css({
+                position : 'absolute',
+                float : 10000,
+                left : 0,
+                top : 0,
+                width : '100%',
+                height : '100%',
+                // border : '1px solid red',
+                background : 'blue'
+              })
+              .appendTo(frameEl.contents().find('body'))
+
+            iEl.css({visibility : 'visible'})
           })
           .attr('src', '/services/trackers0/2013-allied-forces-team')
       }
