da7c4c0be49b5821f263b0ecc23c0d7a7c2ffc16
Michael Ahlers <michael@ahlers.co>
2013-06-08 14:43:58 -0400

    WIP commit.

--- applications/desktop/public/controllers/Trackers.js
+++ applications/desktop/public/controllers/Trackers.js
@@ -6,6 +6,11 @@ define([
 ], function (controllers) {
 
   return controllers.controller('Trackers', function ($rootScope, $scope, $log) {
+    $scope.settings = {}
+
+    $scope.$watch('settings.team', function (team) {
+      $log.log(team)
+    })
   })
 
 })
