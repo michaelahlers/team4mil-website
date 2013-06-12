01f93580a41cf3061ba9b8a1962b9891fdaf1cb1
Michael Ahlers <michael@ahlers.co>
2013-06-11 21:32:34 -0400

    Provide the trackers to the client, and allow selection.

--- applications/desktop/public/controllers/Trackers.js
+++ applications/desktop/public/controllers/Trackers.js
@@ -2,14 +2,21 @@
 
 define([
   'controllers'
+  , 'services/Trackers'
   , 'directives/t4m-tracker-map'
 ], function (controllers) {
 
-  return controllers.controller('Trackers', function ($rootScope, $scope, $log) {
+  return controllers.controller('Trackers', function ($rootScope, $scope, $log, Trackers) {
     $scope.settings = {}
 
     $scope.$watch('settings.team', function (team) {
       $log.log(team)
+    })
+
+    $scope.trackers = Trackers.query().$then(function (response) {
+      var trackers = response.data
+      $scope.settings.tracker = (trackers || [])[0]
+      return trackers
     })
   })
 
