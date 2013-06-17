2b2a8a0d744082fab1b28a42be2432d7cda65415
Michael Ahlers <michael@ahlers.co>
2013-06-17 11:04:12 -0400

    Yep, still fighting IE.

--- applications/desktop/public/directives/t4m-tracker-map.js
+++ applications/desktop/public/directives/t4m-tracker-map.js
@@ -47,30 +47,31 @@ define([
                 , googleMapEl = spotMapEl.children()
 
               //spotHeadEl.append('<style type="text/css">.ext-el-mask, .x-mask-loading { display: none ! important; }</style>')
-              spotBodyEl.children().css({visibility : 'hidden'})
-
-              spotMapEl
-                .css({
-                  position : 'absolute',
-                  float : 1000,
-                  left : 0,
-                  top : 0,
-                  width : '100%',
-                  height : '100%',
-                  border : 'none',
-                  background : 'gray'
-                  //,visibility : 'hidden'
-                })
-                .appendTo(spotBodyEl)
+              spotBodyEl.children().css({display : 'none'})
 
               /* This callback is triggered from outside the Angular digest cycle. */
               scope.$apply(function () {
                 $timeout(function () {
 
+                  spotMapEl
+                    .appendTo(spotBodyEl)
+                    .css({
+                      display : 'block',
+                      position : 'absolute',
+                      float : 1000,
+                      left : 0,
+                      top : 0,
+                      width : '100%',
+                      height : '100%',
+                      border : 'none',
+                      background : 'gray'
+                      //,visibility : 'hidden'
+                    })
+
                   // googleMapEl.css({width : '100%'})
                   // spotMapEl.css({visibility : 'visible'})
                   frameEl.css({visibility : 'visible'})
-                  //iEl.css({visibility : 'visible'})
+                  // iEl.css({visibility : 'visible'})
 
                   $rootScope.$broadcast('t4m-loadingSuccess')
 
