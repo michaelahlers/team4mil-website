2435915e24a852f0da3539f96aefbe16f42d2f11
Michael Ahlers <michael@ahlers.co>
2013-06-17 10:52:44 -0400

    Trying to work-around a bug with IE.

--- applications/desktop/public/directives/t4m-tracker-map.js
+++ applications/desktop/public/directives/t4m-tracker-map.js
@@ -10,7 +10,7 @@ define([
     return {
       restrict : 'E',
       replace : true,
-      template : '<div style="margin: 0; padding: 0; width: 100%; height: 100%; overflow: hidden; visibility: hidden;" />',
+      template : '<div style="margin: 0; padding: 0; width: 100%; height: 100%; overflow: hidden;" />',
 
       scope : {
         tracker : '=ngModel'
@@ -18,7 +18,7 @@ define([
 
       link : function (scope, iEl, iAttrs, controller) {
         scope.$watch('tracker', function (tracker) {
-          iEl.css({visibility : 'hidden'})
+          //iEl.css({visibility : 'hidden'})
 
           iEl.html('')
 
@@ -28,7 +28,15 @@ define([
 
           $rootScope.$broadcast('t4m-loadingStart')
 
-          var frameEl = $('<iframe class="map" style="margin: 0; padding: 0; width: 100%; height: 100%; border: 0;" frameBorder="0" scrolling="no" hspace="0" vspace="0" marginheight="0" marginwidth="0"></iframe>')
+          var frameEl = $('<iframe class="map" frameBorder="0" scrolling="no"></iframe>')
+            .css({
+              margin : 0,
+              padding : 0,
+              width : '100%',
+              height : '100%',
+              border : 0,
+              visibility : 'hidden'
+            })
             .appendTo(iEl)
             .load(function () {
 
@@ -36,29 +44,33 @@ define([
                 , spotHeadEl = spotEl.find('head')
                 , spotBodyEl = spotEl.find('body')
                 , spotMapEl = spotEl.find('#map')
+                , googleMapEl = spotMapEl.children()
 
-              spotHeadEl.append('<style type="text/css">.ext-el-mask, .x-mask-loading { display: none ! important; }</style>')
+              //spotHeadEl.append('<style type="text/css">.ext-el-mask, .x-mask-loading { display: none ! important; }</style>')
+              spotBodyEl.children().css({visibility : 'hidden'})
+
+              spotMapEl
+                .css({
+                  position : 'absolute',
+                  float : 1000,
+                  left : 0,
+                  top : 0,
+                  width : '100%',
+                  height : '100%',
+                  border : 'none',
+                  background : 'gray',
+                  visibility : 'hidden'
+                })
+                .appendTo(spotBodyEl)
 
               /* This callback is triggered from outside the Angular digest cycle. */
               scope.$apply(function () {
                 $timeout(function () {
 
-                  spotMapEl
-                    .css({
-                      position : 'absolute',
-                      float : 1000,
-                      left : 0,
-                      top : 0,
-                      width : '100%',
-                      height : '100%',
-                      border : 'none',
-                      background : 'gray',
-                      visibility : 'hidden'
-                    })
-                    .appendTo(spotBodyEl)
-
+                  // googleMapEl.css({width : '100%'})
                   spotMapEl.css({visibility : 'visible'})
-                  iEl.css({visibility : 'visible'})
+                  frameEl.css({visibility : 'visible'})
+                  //iEl.css({visibility : 'visible'})
 
                   $rootScope.$broadcast('t4m-loadingSuccess')
 
