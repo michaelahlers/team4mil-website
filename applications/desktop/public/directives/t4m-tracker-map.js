e353b9c7859e9e1a833dcc3364016ce3e6e8cb18
Michael Ahlers <michael@ahlers.co>
2013-06-17 12:01:22 -0400

    Trying to make this more compatible because, well, IE.

--- applications/desktop/public/directives/t4m-tracker-map.js
+++ applications/desktop/public/directives/t4m-tracker-map.js
@@ -6,7 +6,7 @@ define([
   , 'jquery'
 ], function (angular, directives, $) {
 
-  directives.directive('t4mTrackerMap', function ($rootScope, $window, $timeout) {
+  directives.directive('t4mTrackerMap', function ($rootScope, $window, $timeout, $log) {
     return {
       restrict : 'E',
       replace : true,
@@ -20,58 +20,42 @@ define([
         scope.$watch('tracker', function (tracker) {
           //iEl.css({visibility : 'hidden'})
 
-          iEl.html('')
-
           if (!tracker) {
             return
           }
 
           $rootScope.$broadcast('t4m-loadingStart')
 
-          var frameEl = $('<iframe class="map" frameBorder="0" scrolling="no" onload="t4m_tracker_map_onload_fn()"></iframe>')
+          $window.t4m_tracker_map_onload_fn = function () {
 
-          frameEl
-            .css({
-              margin : 0,
-              padding : 0,
-              width : '99%',
-              height : '99%',
-              border : 0,
-              visibility : 'hidden'
-            })
-            .appendTo(iEl)
-
-          $window['t4m_tracker_map_onload_fn'] = function () {
-            //.load(function () {
-
-            var spotEl = frameEl.contents()
+            var frameEl = $(iEl.find('iframe'))
+              , spotEl = frameEl.contents()
               , spotHeadEl = spotEl.find('head')
               , spotBodyEl = spotEl.find('body')
               , spotMapEl = spotEl.find('#map')
-              , googleMapEl = spotMapEl.children()
 
-            //spotHeadEl.append('<style type="text/css">.ext-el-mask, .x-mask-loading { display: none ! important; }</style>')
-            spotBodyEl.children().css({display : 'none'})
-            spotBodyEl.append(spotMapEl)
-            spotMapEl.css({display : 'block', visibility : 'visible'})
+            spotHeadEl.append($('<style type="text/css">.x-panel, .x-border-panel { visibility: hidden; }</style>'))
+            spotMapEl.appendTo(spotBodyEl)
 
             /* This callback is triggered from outside the Angular digest cycle. */
             scope.$apply(function () {
               $timeout(function () {
 
-                spotMapEl
-                  .css({
-                    display : 'block',
-                    position : 'absolute',
-                    float : 1000,
-                    left : 0,
-                    top : 0,
-                    width : '100%',
-                    height : '100%',
-                    border : 'none',
-                    background : 'gray'
-                    //,visibility : 'hidden'
-                  })
+//                spotBodyEl.children().css({display : 'none'})
+
+//                spotMapEl                  .appendTo(spotBodyEl)
+//                  .css({
+//                    display : 'block'
+//                    position : 'absolute',
+//                    float : 1000,
+//                    left : 0,
+//                    top : 0,
+//                    width : '100%',
+//                    height : '100%',
+//                    border : 'none',
+//                    background : 'gray'
+//                    visibility : 'visible'
+//                  })
 
                 // googleMapEl.css({width : '100%'})
                 // spotMapEl.css({visibility : 'visible'})
@@ -87,9 +71,8 @@ define([
               }, 500)
             })
           }
-          //})
 
-          frameEl.attr('src', '/services/trackers0/' + tracker.id)
+          iEl.html('<iframe class="map" style="margin: 0; padding: 0; width: 99%; height: 99%; border: 0; visibility: hidden;" frameBorder="0" scrolling="no" onload="t4m_tracker_map_onload_fn()" src="/services/trackers0/' + tracker.id + '"></iframe>')
 
         })
       }
