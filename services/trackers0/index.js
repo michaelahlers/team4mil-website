3145f51b8e36e16c7af919417d956de339fee1f2
Michael Ahlers <michael@ahlers.co>
2013-06-18 10:35:05 -0400

    Let us get the RAAM route coordinates.

--- services/trackers0/index.js
+++ services/trackers0/index.js
@@ -1,8 +1,11 @@
 /*jshint node:true*/
 'use strict'
 
-var express = require('express')
+var csv = require('csv')
+  , express = require('express')
   , environment = require('../../environment')
+  , fs = require('fs')
+  , path = require('path')
   , request = require('request')
   , Q = require('q')
 
@@ -17,6 +20,27 @@ var trackers0 = express()
 
 trackers0.configure(function () {
   /* Empty for now. */
+})
+
+trackers0.get('/2013-raam-route', function (req, res) {
+
+  var coordinates = []
+
+  csv()
+    .from.stream(fs.createReadStream(path.join(__dirname, '2013-raam-route.csv')))
+    .on('record', function (row, index) {
+      /* If the mode is not defined, then default it to the environment
+       * setting so that the condition will pass. */
+      coordinates.push({
+        lat : row.shift(),
+        long : row.shift(),
+        name : row.shift()
+      })
+    })
+    .on('end', function () {
+      res.send(coordinates)
+    })
+
 })
 
 trackers0.get('/', function (req, res) {
