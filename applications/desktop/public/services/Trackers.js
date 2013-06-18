e259ee9af34e03a449c80d1eb59eaef82725489f
Michael Ahlers <michael@ahlers.co>
2013-06-18 15:42:26 -0400

    Additional lifecycle hooks.

--- applications/desktop/public/services/Trackers.js
+++ applications/desktop/public/services/Trackers.js
@@ -3,15 +3,39 @@
 define([
   'services', 'angular-resource'
 ], function (services) {
-  services.factory('Trackers', function ($q, $resource, $http, $log) {
+  services.factory('Trackers', function ($rootScope, $q, $resource, $http, $timeout) {
 
-    var routePoints = $http.get('/services/trackers0/2013-raam-route').then(function (response) {
+    var route = $http.get('/services/trackers0/2013-raam-route').then(function (response) {
       return response.data
     })
 
+    var activeTracker
+
     return {
-      getRoutePoints : function () {
-        return routePoints
+      getRoute : function () {
+        return route
+      },
+
+      monitor : function (tracker) {
+        activeTracker = tracker
+
+        if (!tracker || !tracker.id) {
+          $rootScope.$broadcast('t4m-trackers-stopped')
+          return
+        }
+
+        $rootScope.$broadcast('t4m-trackers-started', tracker)
+
+        var monitor = function () {
+          if (tracker == activeTracker) {
+            $http.get('/services/trackers0/' + tracker.id).then(function (response) {
+              $rootScope.$broadcast('t4m-trackers-update', response.data)
+            })
+            $timeout(monitor, 10000)
+          }
+        }
+
+        monitor()
       }
     }
   })
