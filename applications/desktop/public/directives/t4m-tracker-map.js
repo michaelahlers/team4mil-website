d3ffae4107c19ea1361888e52961417033fb870e
Michael Ahlers <michael@ahlers.co>
2013-06-18 15:35:50 -0400

    Set the map bounds to the start and finish positions.

--- applications/desktop/public/directives/t4m-tracker-map.js
+++ applications/desktop/public/directives/t4m-tracker-map.js
@@ -24,10 +24,6 @@ define([
     return jQuery.map(points, toLatLng)
   }
 
-  var toDistance = function (point0, point1) {
-    return Math.sqrt(Math.pow(point0.latitude - point1.latitude, 2) + Math.pow(point0.longitude - point1.longitude, 2))
-  }
-
   directives.directive('t4mTrackerMap', function ($rootScope, $http, $resource, $q, $timeout, Trackers, $log) {
     var feed = $resource('/services/trackers0/:id')
 
@@ -41,19 +37,7 @@ define([
       },
 
       link : function (scope, iEl, iAttrs, controller) {
-        $rootScope.$broadcast('t4m-loadingStart')
-
-        var getDefaultViewport = function () {
-          var deferred = $q.defer()
-
-          geocoder.geocode({'address' : 'US'}, function (results, status) {
-            scope.$apply(function () {
-              deferred.resolve(results[0].geometry.viewport)
-            })
-          })
-
-          return deferred.promise
-        }
+        //$rootScope.$broadcast('t4m-loadingStart')
 
         var map = new maps.Map(iEl[0], {
           mapTypeId : maps.MapTypeId.TERRAIN,
@@ -62,18 +46,16 @@ define([
           zoomControl : true
         })
 
-        getDefaultViewport().then(function (viewport) {
-          map.fitBounds(viewport)
-        })
+        Trackers.getRoute().then(function (route) {
+          var points = route.points
+            , start = toLatLng(points[0])
+            , finish = toLatLng(points[points.length - 1])
 
-        Trackers.getRoutePoints().then(function (route) {
-          var coordinates = jQuery.map(route, function (position) {
-            return new maps.LatLng(position.latitude, position.longitude)
-          })
+          map.fitBounds(new maps.LatLngBounds(start, finish))
 
-          geocoder.geocode({'location' : coordinates[0]}, function (results, status) {
+          geocoder.geocode({'location' : start}, function (results, status) {
             var marker = new maps.Marker({
-              position : coordinates[0],
+              position : start,
               map : map
             })
 
@@ -83,9 +65,9 @@ define([
             })
           })
 
-          geocoder.geocode({'location' : coordinates[coordinates.length - 1]}, function (results, status) {
+          geocoder.geocode({'location' : finish}, function (results, status) {
             var marker = new maps.Marker({
-              position : coordinates[coordinates.length - 1],
+              position : finish,
               map : map
             })
 
@@ -97,92 +79,53 @@ define([
         })
 
         scope.$watch('tracker', function (tracker) {
-          if (!tracker) {
-            delete scope.messages
-            return
-          }
-
-          var monitor = function () {
-            if (tracker == scope.tracker) {
-              scope.messages = feed.get({ id : tracker.id}).$then(function (response) {
-                $rootScope.$broadcast('t4m-loadingSuccess')
-                return response.data.response.feedMessageResponse.messages.message
-              })
-              $timeout(monitor, 1000 * 60)
-            }
-          }
-          monitor()
+          Trackers.monitor(tracker)
         })
 
-        var behindPolyline = new maps.Polyline({
-            strokeColor : '#FF0000',
-            strokeOpacity : 0.75,
-            strokeWeight : 5
-          })
-          , aheadPolyline = new maps.Polyline({
-            strokeColor : '#000000',
-            strokeOpacity : 0.5,
-            strokeWeight : 3
-          })
-          , currentMarker = new maps.Marker({ map : map })
-          , currentPopup = new maps.InfoWindow()
-
-        behindPolyline.setMap(map)
-        aheadPolyline.setMap(map)
-
-        maps.event.addListener(currentMarker, 'click', function () {
-          currentPopup.open(map, currentMarker)
-          map.panTo(currentMarker.getPosition())
+        scope.$on('t4m-trackers-update', function (event, update) {
+          $log.log(update)
         })
 
-        var getProgress = function (referencePoint) {
-          var deferred = $q.defer()
-
-          Trackers.getRoutePoints().then(function (points) {
-            var shortestDistance = toDistance(referencePoint, points[0])
-              , closestIndex = 0
-
-            for (var index = 1; index < points.length; index++) {
-              var distance = toDistance(referencePoint, points[index])
-
-              if (distance < shortestDistance) {
-                closestIndex = index
-                shortestDistance = distance
-              }
-            }
-
-            deferred.resolve({
-              index : closestIndex,
-              total : points.length,
-              all : points,
-              behind : points.slice(0, closestIndex),
-              ahead : points.slice(closestIndex, points.length - 1),
-              resolved : points[closestIndex],
-              reference : referencePoint
-            })
-          })
-
-          return deferred.promise
-        }
-
-        scope.$watch('messages', function (messages) {
-          if (!messages) {
-            return
-          }
-
-          var currentCoordinate = toLatLng(messages[0])
-
-          currentMarker.setPosition(currentCoordinate)
-
-          getProgress(messages[0]).then(function (progress) {
-            behindPolyline.setPath(toLatLngs(progress.behind).concat([currentCoordinate]))
-            aheadPolyline.setPath([currentCoordinate].concat(toLatLngs(progress.ahead)))
-          })
-
-          geocoder.geocode({'location' : currentCoordinate}, function (results, status) {
-            currentPopup.setContent(results[0].formatted_address)
-          })
-        })
+//        var behindPolyline = new maps.Polyline({
+//            strokeColor : '#FF0000',
+//            strokeOpacity : 0.75,
+//            strokeWeight : 5
+//          })
+//          , aheadPolyline = new maps.Polyline({
+//            strokeColor : '#000000',
+//            strokeOpacity : 0.5,
+//            strokeWeight : 3
+//          })
+//          , currentMarker = new maps.Marker({ map : map })
+//          , currentPopup = new maps.InfoWindow()
+//
+//        behindPolyline.setMap(map)
+//        aheadPolyline.setMap(map)
+//
+//        maps.event.addListener(currentMarker, 'click', function () {
+//          currentPopup.open(map, currentMarker)
+//          map.panTo(currentMarker.getPosition())
+//        })
+//
+//
+//        scope.$watch('messages', function (messages) {
+//          if (!messages) {
+//            return
+//          }
+//
+//          var currentCoordinate = toLatLng(messages[0])
+//
+//          currentMarker.setPosition(currentCoordinate)
+//
+//          getProgress(messages[0]).then(function (progress) {
+//            behindPolyline.setPath(toLatLngs(progress.behind).concat([currentCoordinate]))
+//            aheadPolyline.setPath([currentCoordinate].concat(toLatLngs(progress.ahead)))
+//          })
+//
+//          geocoder.geocode({'location' : currentCoordinate}, function (results, status) {
+//            currentPopup.setContent(results[0].formatted_address)
+//          })
+//        })
       }
     }
   })
