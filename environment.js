f931058d2d0d0db8960df9ca71768fa256755e6b
Michael Ahlers <michael@ahlers.co>
2015-09-01 10:17:56 -0400

    Make GitHub API key trollers sad.

--- environment.js
+++ environment.js
@@ -1,8 +1,10 @@
 /*jshint node:true*/
 'use strict'
 
+/* Defunct as of September 1, 2015. */
 exports.MAILGUN_API_KEY = process.env.MAILGUN_API_KEY || 'key-4vqyrivdws3fvkaj2d391ict3kbsinb5'
 
+/* Defunct as of September 1, 2015. */
 exports.STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY || 'sk_test_G5yGsRwgUd2kdLH8ohyrKopm'
 exports.STRIPE_PUBLISHABLE_KEY = process.env.STRIPE_PUBLISHABLE_KEY || 'pk_test_2R7rJ6BRwTCbW6SW21jAGVr1'
 
